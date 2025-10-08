604f6dd40ad44c75c76ccaf26607b1d8a95111c4
biplobrana007 <mubiplobrana@gmail.com>
2025-10-08 13:01:04 +0600

    footer is done

--- src/Layouts/Root.jsx
+++ src/Layouts/Root.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
-import { Outlet } from 'react-router';
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import { Outlet } from "react-router";
 
 const Root = () => {
-    return (
-        <div>
-           <Navbar></Navbar>
-           <Outlet></Outlet>
-           <Footer></Footer>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div className="min-h-screen">
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Root;
